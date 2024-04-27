dcc692cf640b8107892d27c9db9d9eba428a607c
lapikag <lapikag@gmail.com>
2024-04-27 15:31:17 +0200

    add lesson1,task4

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 10;
+int b = 20;
+int c = 30;
+int d = 120;
+int e = 5; 
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
