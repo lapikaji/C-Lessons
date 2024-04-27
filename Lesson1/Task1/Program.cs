ff08a2201acda32926be145194f81414d4c19317
lapikag <lapikag@gmail.com>
2024-04-27 13:59:00 +0200

    new lines

--- Lesson1/Task1/Program.cs
+++ Lesson1/Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Brother, ewwwww, whats dat?");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d=5.555;
+char c= '!';
+bool b=true;
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(b);
+
--- Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Lesson1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03c7859fea8ef8d4bd15ef3c6ec39b797f202bcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0180b839be217752873117bd77e8591ec5662a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
