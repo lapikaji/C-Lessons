1bdf0684b3e4408171eb4d38a55324f5bf1e405d
lapikag <lapikag@gmail.com>
2024-04-27 15:16:31 +0200

    add lesson1,task2

--- Lesson1/task2/Program.cs
+++ Lesson1/task2/Program.cs
@@ -3,7 +3,7 @@ int i2 = 6;
 int sum = i1 + i2;
 Console.WriteLine(sum);
 
-long l1 = 10;
+long l1 = 6;
 long sum2 = l1 + i1;
 Console.WriteLine(sum2);
 
@@ -16,3 +16,10 @@ double product3 = i1 * d1;
 Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
+
+int quotient = i1/i2;
+Console.WriteLine(quotient);
+
+d1 = 6;
+double quotient2 = i1/d1;
+Console.WriteLine(quotient2);
