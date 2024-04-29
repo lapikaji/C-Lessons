1e89dd1b910d5a07af738b6d84f1f53437568702
lapikag <lapikag@gmail.com>
2024-04-29 20:21:02 +0200

    add lesson2,task4

---
+++ Lesson2/task4/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 5;
+int[] array = { 2, 3, 4, 5, 56 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+
+Console.WriteLine(max);
