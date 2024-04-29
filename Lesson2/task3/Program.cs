9a8271ba12a176b36506634ade0a01a770d0443a
lapikag <lapikag@gmail.com>
2024-04-29 20:07:17 +0200

    add lesson2,task3

---
+++ Lesson2/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = 10;
+int[] array = { 2, 34, 56, 557, 23, 13, 2, 5, 7, 68 };
+int i = 0;
+
+while (i < N)
+{
+    if (array[i] % 2 == 0)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
