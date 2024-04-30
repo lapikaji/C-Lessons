4b4c6e03a25d9acb4bb515d4c6bbfa32a7f75e90
lapikag <lapikag@gmail.com>
2024-04-30 19:52:32 +0200

    add Lesson3,task3

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Функции - блок кода, решающий определенную задачу
+
+// int Square(int value);
+// {
+// int result = value * value;
+//return result;
+// }
+
+// Термины Функция и Метод - одно и то же в С# 
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double nomenator = a * b;
+    int denomenator = c + d;
+    double result = nomenator / denomenator;
+    return result;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
