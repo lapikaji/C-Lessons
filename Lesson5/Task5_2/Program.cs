f7546ba2843b947c9d2651d53b8120cce95946e5
lapikag <lapikag@gmail.com>
2024-05-03 20:03:56 +0200

    updated Lesson5

--- Lesson5/Task5_2/Program.cs
+++ Lesson5/Task5_2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+public class Answer
+{
+    public int FindMax(int num1, int num2, int num3)
+    {
+        int max = num1;
+
+        if (num2 > max)
+        {
+            max = num2;
+        }
+
+        if (num3 > max)
+        {
+            max = num3;
+        }
+
+        return max;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Answer answer = new Answer();
+        int maxNumber = answer.FindMax(5, 10, 3);
+        Console.WriteLine($"Максимальное число: {maxNumber}");
+    }
+}
---
+++ Lesson5/Task5_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+        for (int i = 2; i <= number; i += 2)
+        {
+            Console.Write($"{i} ");
+        }
+        Console.WriteLine();
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 5;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
